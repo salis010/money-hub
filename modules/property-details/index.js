71e18289f477a489a19a0216d6dae83432c5777d
salis010 <stephen.saliba@gmail.com>
2022-03-05 13:34:27 +0100

    Uses api to get account details

--- modules/property-details/index.js
+++ modules/property-details/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-statements */
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { add, format } from "date-fns";
 import { Button } from "../../components/button";
 import RowContainer from "../../components/row-container";
@@ -7,104 +7,90 @@ import {
   AccountHeadline, AccountLabel, AccountList, AccountListItem, AccountSection, InfoText, Inset
 } from "./style";
 
-
-const account = {
-  uid: "65156cdc-5cfd-4b34-b626-49c83569f35e",
-  deleted: false,
-  dateCreated: "2020-12-03T08:55:33.421Z",
-  currency: "GBP",
-  name: "15 Temple Way",
-  bankName: "Residential",
-  type: "properties",
-  subType: "residential",
-  originalPurchasePrice: 250000,
-  originalPurchasePriceDate: "2017-09-03",
-  recentValuation: { amount: 310000, status: "good" },
-  associatedMortgages: [
-    {
-      name: "HSBC Repayment Mortgage",
-      uid: "fb463121-b51a-490d-9f19-d2ea76f05e25",
-      currentBalance: -175000,
-    },
-  ],
-  canBeManaged: false,
-  postcode: "BS1 2AA",
-  lastUpdate: "2020-12-01T08:55:33.421Z",
-  updateAfterDays: 30,
-};
-
 const Detail = ({}) => {
+  const [ account, setAccount ] = useState()
+  const [ lastUpdate, setLastUpdate ] = useState()
+  const [ mortgage, setMortgage ] = useState()
+  
   useEffect(() => {
-    window
-      .fetch("/api/account")
+    window.fetch("/api/account")
       .then((res) => res.json())
-      .then(console.log);
+      .then(data => {
+        const accountDetails = data.account
+
+        setLastUpdate(new Date(accountDetails.lastUpdate))
+
+        const mortgageDetails = accountDetails.associatedMortgages.length ? accountDetails.associatedMortgages[0] : undefined
+        setMortgage(mortgageDetails)
+        
+        setAccount(accountDetails)
+      })
   }, []);
 
-  const lastUpdate = new Date(account.lastUpdate);
-  
-  const mortgage = account.associatedMortgages.length ? account.associatedMortgages[0] : undefined
-
   return (
-    <Inset>
-      <AccountSection>
-        <AccountLabel>Estimated Value</AccountLabel>
-        <AccountHeadline>
-          {new Intl.NumberFormat("en-GB", {
-            style: "currency",
-            currency: "GBP",
-          }).format(account.recentValuation.amount)}
-        </AccountHeadline>
-        <AccountList>
-          <AccountListItem><InfoText>
-            {`Last updated ${format(lastUpdate, "do MMM yyyy")}`}
-          </InfoText></AccountListItem>
-          <AccountListItem><InfoText>
-            {`Next update ${format(
-              add(lastUpdate, { days: account.updateAfterDays }),
-              "do MMM yyyy"
-            )}`}
-          </InfoText></AccountListItem>
-        </AccountList>
-      </AccountSection>
-      <AccountSection>
-        <AccountLabel>Property details</AccountLabel>
-        <RowContainer>
-          <AccountList>
-            <AccountListItem><InfoText>{account.name}</InfoText></AccountListItem>
-            <AccountListItem><InfoText>{account.bankName}</InfoText></AccountListItem>
-            <AccountListItem><InfoText>{account.postcode}</InfoText></AccountListItem>
-          </AccountList>
-        </RowContainer>
-      </AccountSection>
-      {mortgage && (
-        <AccountSection>
-          <AccountLabel>Mortgage</AccountLabel>
-          <RowContainer
-            // This is a dummy action
-            onClick={() => alert("You have navigated to the mortgage page")}
-          >
+    <>
+      {!account && <p>Loading...</p>}
+      {account &&
+        <Inset>
+          <AccountSection>
+            <AccountLabel>Estimated Value</AccountLabel>
+            <AccountHeadline>
+              {new Intl.NumberFormat("en-GB", {
+                style: "currency",
+                currency: "GBP",
+              }).format(account.recentValuation.amount)}
+            </AccountHeadline>
             <AccountList>
               <AccountListItem><InfoText>
-                {new Intl.NumberFormat("en-GB", {
-                  style: "currency",
-                  currency: "GBP",
-                }).format(
-                  Math.abs(account.associatedMortgages[0].currentBalance)
-                )}
+                {`Last updated ${format(lastUpdate, "do MMM yyyy")}`}
               </InfoText></AccountListItem>
-              <AccountListItem><InfoText>{account.associatedMortgages[0].name}</InfoText></AccountListItem>
+              <AccountListItem><InfoText>
+                {`Next update ${format(
+                  add(lastUpdate, { days: account.updateAfterDays }),
+                  "do MMM yyyy"
+                )}`}
+              </InfoText></AccountListItem>
             </AccountList>
-          </RowContainer>
-        </AccountSection>
-      )}
-      <Button
-        // This is a dummy action
-        onClick={() => alert("You have navigated to the edit account page")}
-      >
-        Edit account
-      </Button>
-    </Inset>
+          </AccountSection>
+          <AccountSection>
+            <AccountLabel>Property details</AccountLabel>
+            <RowContainer>
+              <AccountList>
+                <AccountListItem><InfoText>{account.name}</InfoText></AccountListItem>
+                <AccountListItem><InfoText>{account.bankName}</InfoText></AccountListItem>
+                <AccountListItem><InfoText>{account.postcode}</InfoText></AccountListItem>
+              </AccountList>
+            </RowContainer>
+          </AccountSection>
+          {mortgage && (
+            <AccountSection>
+            <AccountLabel>Mortgage</AccountLabel>
+            <RowContainer
+              // This is a dummy action
+              onClick={() => alert("You have navigated to the mortgage page")}
+            >
+              <AccountList>
+                <AccountListItem><InfoText>
+                  {new Intl.NumberFormat("en-GB", {
+                    style: "currency",
+                    currency: "GBP",
+                  }).format(
+                    Math.abs(account.associatedMortgages[0].currentBalance)
+                  )}
+                </InfoText></AccountListItem>
+                <AccountListItem><InfoText>{account.associatedMortgages[0].name}</InfoText></AccountListItem>
+              </AccountList>
+            </RowContainer>
+          </AccountSection>
+          )}
+          <Button
+            // This is a dummy action
+            onClick={() => alert("You have navigated to the edit account page")}
+          >
+            Edit account
+          </Button>
+        </Inset>}
+    </>
   );
 };
 
